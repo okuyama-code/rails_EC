560ccca60aa83e43e94b77cc4e2f5e7215b8a48c
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-09-19 16:48:59 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'products/index'
+  root "tasks#index"
+
   resources :tasks
-  resources :products
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
 end
