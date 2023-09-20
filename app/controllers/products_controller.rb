4b2b794b0ed6dfbecb9fce5badf9cc86cd22bcd4
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-09-20 09:34:18 +0900

    商品一覧、詳細機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,46 @@
 class ProductsController < ApplicationController
-  def index
-  end
+  before_action :set_product, only: %i[show edit update]
 
-  def show
+  def index
+    @products = Product.all
+    @products = Product.order(created_at: :desc)
   end
 
   def new
+    @product = Product.new
   end
 
-  def edit
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @products = Product.all
+    @products = Product.order(created_at: :desc)
+  end
+
+  def edit; end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock, :image)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,40 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+p '==================== product create ===================='
+
+product1 = Product.new(
+    name: "黄色シューズ",
+    description: "ランニングシューズメンズ スポーツシューズ メッシュ 軽量 通気性",
+    price: 10000,
+    stock: 30,
+)
+product1.image.attach(io: File.open(Rails.root.join('app/assets/images/EC1.jpg')), filename: 'EC1.jpg')
+product1.save!
+
+product2 = Product.new(
+    name: "赤シューズ",
+    description: "ランニングシューズメンズ スポーツシューズ メッシュ 赤色",
+    price: 7500,
+    stock: 20,
+)
+
+product2.image.attach(io: File.open(Rails.root.join('./app/assets/images/EC2.jpg')), filename: 'EC2.jpg')
+product2.save!
+
+product3 = Product.new(
+    name: "ピンクシューズ",
+    description: "ランニングシューズメンズ スポーツシューズ メッシュ ピンク色",
+    price: 5500,
+    stock: 10,
+)
+
+product3.image.attach(io: File.open(Rails.root.join('./app/assets/images/EC3.jpg')), filename: 'EC3.jpg')
+product3.save!
+
+product4 = Product.new(
+    name: "青色シューズ",
+    description: "ランニングシューズメンズ スポーツシューズ メッシュ 青色色",
+    price: 500,
+    stock: 10,
+)
+
+product4.image.attach(io: File.open(Rails.root.join('./app/assets/images/EC4.jpg')), filename: 'EC4.jpg')
+product4.save!
