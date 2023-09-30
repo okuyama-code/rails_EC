b237d4936833449b413e93e3131671569fa976b3
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-09-30 12:34:15 +0900

    cart機能の@cart.idなどをviewでひょうじさせることに成功

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,8 +2,8 @@ class Cart < ApplicationRecord
    # 1つのカートは複数のカートアイテム（商品との結びつき）を持つ
   has_many :cart_items
   # 1つのカートは複数の商品を持つ（カートアイテムを介して）
-  has_many :products
+  has_many :products, through: :cart_items
 
-  
+
 end
 
