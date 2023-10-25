b60497a3fcb6c14845a6da2e8a8001ddedbb47c8
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-10-25 18:35:11 +0900

    orders_controller修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,8 +12,9 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(first_name: params[:first_name], last_name: params[:last_name], username: params[:username], email: params[:email], address: params[:address], address_2: params[:address_2], country: params[:country], prefecture: params[:prefecture], credit_card_name: params[:credit_card_name], credit_card_number: params[:credit_card_number], credit_card_expiration: params[:credit_card_expiration], credit_card_cvv: params[:credit_card_cvv], cart_id: session[:cart_id])
     if @order.save
+       # メール送信時にプレビューに渡すデータを指定
+      OrderMailer.with(order: @order).send_order_email.deliver_later
       redirect_to orders_path, notice: "購入者の情報をordersテーブルに保存しました"
-      OrderMailer.send_order_email.deliver_later
       # redirect_to root_path, notice: "ご購入ありがとうございます。"
     else
       flash[:notice] = "DBにデータを追加するのに失敗しています。空欄はありませんか？すべてのフォームに入力してください"
