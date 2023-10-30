17393d956b8c80267c25ecd2e9e44d5b901acbf3
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-10-31 02:12:23 +0900

    orderページのlayout一時的に完成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   # Basic認証
   #TODO http_basic_authenticate_with name: 'admin', password: 'pw'　
+  
   def index
     @orders = Order.all
   end
@@ -16,7 +17,7 @@ class OrdersController < ApplicationController
       OrderMailer.send_order_email(@order).deliver_later
       redirect_to orders_path, notice: "購入者の情報をordersテーブルに保存しました"
       # redirect_to root_path, notice: "ご購入ありがとうございます。"
-      @order.cart.cart_products.all
+      # @order.cart.cart_products.all
 
     else
       flash[:notice] = "DBにデータを追加するのに失敗しています。空欄はありませんか？すべてのフォームに入力してください"
