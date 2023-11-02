4fec832328f81f090a1598e6eb4b4a17549dd372
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-11-02 19:05:22 +0900

    production修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { protocol: 'https', host:'peaceful-waters-90263-196a9ea53a5c.herokuapp.com'}
 end
